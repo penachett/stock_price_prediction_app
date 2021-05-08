08439c6951d0046246177f202617bfbdfa00d599
Baklanov Lev <penachett@bk.ru>
2021-05-08 03:34:06 +0300

    added getting api key from local properties

--- android/app/src/main/java/com/bmstu/stonksapp/MainActivity.kt
+++ android/app/src/main/java/com/bmstu/stonksapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.bmstu.stonksapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, resources.getString(R.string.tinkoff_api_key))
+    }
+
+    companion object {
+        const val TAG = "Main Activity"
     }
 }
