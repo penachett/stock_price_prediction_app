42fb653a0e1b14c16a82e90b649c502ccbaf6e7c
Baklanov Lev <penachett@bk.ru>
2021-05-08 04:24:46 +0300

    added dependencies

--- android/app/src/main/java/com/bmstu/stonksapp/ui/MainActivity.kt
+++ android/app/src/main/java/com/bmstu/stonksapp/ui/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.bmstu.stonksapp
+package com.bmstu.stonksapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.bmstu.stonksapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
