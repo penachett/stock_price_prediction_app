a27f2ac474cb13449f9e9026758658295ed64c0f
Baklanov Lev <penachett@bk.ru>
2021-05-21 03:07:58 +0300

    added structs

--- backend/structs.go
+++ backend/structs.go
@@ -8,3 +8,21 @@ type errorResponse struct {
 type success struct {
 	Success bool `json:"success"`
 }
+
+type user struct {
+	Id int `json:"id"`
+}
+
+type prediction struct {
+	Id int `json:"id"`
+	Ticker string `json:"ticker"`
+	CreateTime string `json:"create_time"`
+	PredictTime string `json:"predict_time"`
+	PredictedPrice string `json:"predicted_price"`
+	UserId string `json:"user_id"`
+}
+
+type infoForPrediction struct {
+	Ticker string `json:"ticker"`
+	ClosePrices []int `json:"close_prices"`
+}
